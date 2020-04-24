de2f911649b74e2aacdd3df7fae7654fa41c8542
gdukes707 <Gdukes707@gmail.com>
2020-04-24 16:30:22 -0700

    adds auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,62 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 
-const db = require("../models");
+const db = require("../models/index");
 
 router.get("/register", (req, res) => {
-  res.render("/auth");
+  res.send("<h1>Well hello</h1>");
+  //   res.render("/auth/register", {
+
+  //   });
 });
+
+router.post("/register", async (req, res) => {
+  try {
+    const trainer = await db.Trainer.findone({ name: req.body.name });
+    if (trainer) {
+      return res.send("<h1>Account exists<h1>");
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const trainerData = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hash,
+    };
+    await db.Trainer.create(trainerData);
+    res.redirect("/auth/login");
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  //   res.render("auth/login");
+  res.send("hella");
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const trainer = await db.Trainer.findone({ name: req.body.name });
+    if (!trainer) {
+      return res.render("auth/login", {
+        error: "Invalid Login Credentials",
+      });
+    }
+    const passwordsMatch = bcrypt.compareSync(
+      req.body.password,
+      trainer.password
+    );
+    if (!passwordsMatch) {
+      return res.render("auth/login", {
+        error: "Invalid Login Credentials",
+      });
+    }
+    res.redirect("/trainer");
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const Pokedex = require("pokedex-promise-v2");
 const P = new Pokedex();
 const app = express();
 
+const authController = require("./controllers/authController");
+
 const db = require("./models");
 
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,6 +21,8 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.use("/auth", authController);
 
 app.listen(port, () =>
   console.log(`Hey pokedex server is on at port: ${port} `)
