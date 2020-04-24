9f142ecbc2cd831c5bc2b948bf84141c853c05f0
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2020-04-24 16:00:00 -0700

    server amended

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
