591f3e2e164628446cb62c1b6501d87a9037ed62
gdukes707 <Gdukes707@gmail.com>
2020-04-24 19:33:40 -0700

    logic of trainer controller set up

--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -1,12 +1,55 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("../models/index");
+
 router.get("/", (req, res) => {
-  res.send("hi me bane");
+  res.redirect("auth/login");
 });
 
-router.get("/show", (req, res) => {
-  res.render;
+router.get("/:id", async (req, res) => {
+  try {
+    const foundTrainer = await db.Trainer.findById(req.params.id).populate(
+      "pokemon"
+    );
+    res.render("trainer/show", {
+      trainer: foundTrainer,
+    });
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const foundTrainer = await db.Trainer.findById(req.params.id);
+    res.render("trainer/edits", {
+      trainer: foundTrainer,
+    });
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const foundTrainer = await db.Trainer.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      res.redirect(`/trainers/${req.params.id}`)
+    );
+  } catch (err) {
+    return res.send(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await db.Trainer.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    return res.send(err);
+  }
 });
 
 module.exports = router;
