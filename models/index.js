669b011e3702a81c85ebccfe0ff4737ecaf349ca
gdukes707 <Gdukes707@gmail.com>
2020-04-27 10:09:27 -0700

    adds pokemon index connection and pokemon show page connection

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost:27017/poke-tracker";
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/poke-tracker";
 const configOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
